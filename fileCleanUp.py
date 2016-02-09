9d90229b2e04e1f2762e35b05d73f50cd0f33b99
lhalstro <logan.halstrom@nasa.gov>
2016-02-09 10:25:56 -0600

    added a delete all except function

--- fileCleanUp.py
+++ fileCleanUp.py
@@ -31,6 +31,17 @@ def Delete(filename):
     # cmd('rm {}'.format(filename))
     cmd( 'ls {}'.format(filename) )
 
+def DeleteIth(dir, header, i):
+    """Within a loop, delete file in given directory with given header
+    for given number"""
+    #Create filename to delete ('header.number')
+    filename = '{}.{}'.format(header, i)
+    pathtodelete = '{}/{}'.format(dir, filename)
+    #DELETE FILE
+    if os.path.isfile(pathtodelete):
+        print('Deleting: {}'.format(filename))
+        Delete(pathtodelete)
+
 def DeleteSeries(dir, header, istart, iend, incr=1):
     """Delete a series of files of given file header withing the number range
     specified.
@@ -40,26 +51,43 @@ def DeleteSeries(dir, header, istart, iend, incr=1):
     """
     todelete = np.append( np.arange(istart, iend, incr), iend )
     for i in todelete:
-        #Create filename to delete ('header.number')
-        filename = '{}.{}'.format(header, i)
-        pathtodelete = '{}/{}'.format(dir, filename)
-        #DELETE FILE
-        try:
-            print('Deleting: {}'.format(filename))
-            Delete(pathtodelete)
-        except Exception:
-            pass
+        DeleteIth(dir, header, i)
 
-def DeleteExcept():
+        # #Create filename to delete ('header.number')
+        # filename = '{}.{}'.format(header, i)
+        # pathtodelete = '{}/{}'.format(dir, filename)
+        # #DELETE FILE
+        # try:
+        #     print('Deleting: {}'.format(filename))
+        #     Delete(pathtodelete)
+        # except Exception:
+        #     pass
+
+def DeleteExcept(dir, header, istart, iend, incr=1):
     """Within given range, delete everything EXCEPT the specified range"""
+    #GIVEN INPUTS SAVE ALL FILES WITHIN RANGE
+    if incr == 1:
+        print('\nNO FILES WILL BE DELETED IN THIS SERIES\n')
+        return
+    #DELETE EVERY FILE NOT WITHIN GIVEN SERIES TO SAVE
+    tosave = np.append( np.arange(istart, iend, incr), iend )
+    for i in np.append( np.arange(istart, iend, 1), iend ):
+        if not i in tosave:
+            DeleteIth(dir, header, i)
 
 
 
-def main(dir, headers, istart, iend, incr=1):
+def main(dir, headers, istart, iend, incr=1, allbut=False):
 
     #DELETE SERIES FOR EACH FILE HEADER
     for head in headers:
-        DeleteSeries(dir, head, istart, iend, incr)
+        if allbut:
+            #DELETE ALL FILES WITHIN RANGE EXCEPT SPECIFIED SERIES
+            DeleteExcept(dir, head, istart, iend, incr)
+        else:
+            #DELETE ONLY FILES IN SPECIFIED SERIES
+            DeleteSeries(dir, head, istart, iend, incr)
+
 
 
 
