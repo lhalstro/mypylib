6a3210e519206534a2662d6304524cff31962136
lhalstro <logan.halstrom@nasa.gov>
2016-02-09 14:55:14 -0600

    edits after cleaning out directories

--- fileCleanUp.py
+++ fileCleanUp.py
@@ -28,8 +28,8 @@ import numpy as np
 
 def Delete(filename):
     """Delete a given file"""
-    # cmd('rm {}'.format(filename))
-    cmd( 'ls {}'.format(filename) )
+    cmd('rm {}'.format(filename))
+    # cmd( 'ls {}'.format(filename) )
 
 def DeleteIth(dir, header, i):
     """Within a loop, delete file in given directory with given header
@@ -52,16 +52,6 @@ def DeleteSeries(dir, header, istart, iend, incr=1):
     todelete = np.append( np.arange(istart, iend, incr), iend )
     for i in todelete:
         DeleteIth(dir, header, i)
-
-        # #Create filename to delete ('header.number')
-        # filename = '{}.{}'.format(header, i)
-        # pathtodelete = '{}/{}'.format(dir, filename)
-        # #DELETE FILE
-        # try:
-        #     print('Deleting: {}'.format(filename))
-        #     Delete(pathtodelete)
-        # except Exception:
-        #     pass
 
 def DeleteExcept(dir, header, istart, iend, incr=1):
     """Within given range, delete everything EXCEPT the specified range"""
@@ -93,10 +83,32 @@ def main(dir, headers, istart, iend, incr=1, allbut=False):
 
 if __name__ == "__main__":
 
-    dir = '/lustre2/work/lhalstro/parachuteProject/solutions/pendulum/dev1/convergStudy/timesens35deg/m0.15a180.0_0.25dt5sub'
-    headers = ['x.y0', 'q.y0', 'x.surf', 'q.surf']
-    istart = 19200
-    iend = 20208
-    incr = 32
+    #RUN DIRECTORY
+    dir = '/lustre2/work/lhalstro/parachuteProject/solutions/pendulum/dev1/convergStudy/timesens35deg/m0.15a180.0_0.5dt5sub'
+    dir = '/lustre2/work/lhalstro/parachuteProject/solutions/pendulum/dev1/dynamicRuns/m15/m0.15a180.0_10deg'
 
-    main(dir, headers, istart, iend, incr)
+
+    # cases = ['0.5dt10sub', '1dt10sub', '2.5dt10sub', '2.5dt15sub', '2.5dt5sub', '5dt10sub']
+    # dir = '/lustre2/work/lhalstro/parachuteProject/solutions/pendulum/pendulum2014/pendulum_runs/timesens_4deg/m0.15a180.0_'
+    # for case in cases:
+    #     dir = '/lustre2/work/lhalstro/parachuteProject/solutions/pendulum/pendulum2014/pendulum_runs/timesens/m0.15a180.0_'
+    #     dir = dir + case
+
+    dir = '/lustre2/work/lhalstro/parachuteProject/solutions/pendulum/pendulum2014/pendulum_runs/trialruns/m0.15a180.0_zeroStart'
+
+    #SOLUTION SLICES
+    headers = ['x.y0', 'q.y0', 'x.surf', 'q.surf']
+    # headers = [ 'q.y0', 'q.surf']
+    # headers = ['x.y0', 'q.y0', 'x.surf']
+    headers = ['x.surf','q.surf']
+
+    #SOLUTION RESTART FILES
+    headers = ['x', 'q']
+    # # headers = ['q']
+
+    #DELETE/SAVE INTERVAL
+    istart = 1000
+    iend = 9000
+    incr = 10
+
+    main(dir, headers, istart, iend, incr, allbut=True)
