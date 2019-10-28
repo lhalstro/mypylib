cdfe4fd7abe65c1951a496e59da52140ff3f4cda
ldhalstrom <loganhalstrom@gmail.com>
2019-10-28 15:36:54 -0700

    add script to provide unit conversions

---
+++ unitconvert.py
@@ -0,0 +1,33 @@
+"""UNIT CONVERSIONS
+Logan Halstrom
+CREATED:  28 OCT 2019
+MODIFIED: 28 OCT 2019
+
+
+DESCRIPTION:  provides unit conversions
+
+TO IMPORT:
+import sys
+import os
+HOME = os.path.expanduser('~')
+sys.path.append('{}/lib/python'.format(HOME))
+import unitconverter as uc
+"""
+
+
+
+#DISTANCE
+
+#feet/meters
+ft2m = 0.3048
+m2ft = 1/ft2m
+#miles/feet
+mi2ft = 5280
+ft2mi = 1/mi2ft
+
+
+#MASS
+
+#kilograms/pounds
+kg2lb = 2.20462
+
