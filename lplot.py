5d0c97b6619a2e1a56dcec52f22fa7598a6f6345
Logan Halstrom <logan.halstrom@nasa.gov>
2022-05-24 21:47:26 -0700

    add `linestyle_names` dict for linestyles that require a tuple input

--- lplot.py
+++ lplot.py
@@ -195,7 +195,26 @@ bigmarkers = ['o', 'v', 'd', 's', '*', 'D', 'p', '>', 'H', '8']
 bigdotmarkers = ['$\\odot$', 'v', 'd', '$\\boxdot$', '*', 'D', 'p', '>', 'H', '8']
 scattermarkers = ['o', 'v', 'd', 's', 'p']
 
-linestyles = ['-', '--', '-.', '.']
+linestyles = ['-', '--', '-.', '.'] #linestyle sequencer
+#more named linestyles that must be input as a tuple
+linestyle_names = {
+    'solid'                  :  'solid'                    ,
+    'dotted'                 :  'dotted'                   ,
+    'dashed'                 :  'dashed'                   ,
+    'dashdot'                :   'dashdot'                 ,
+    'loosely dotted'         :  (0, (1, 10))               ,
+    'dotted'                 :  (0, (1, 1))                ,
+    'densely dotted'         :  (0, (1, 1))                ,
+    'loosely dashed'         :  (0, (5, 10))               ,
+    'dashed'                 :  (0, (5, 5))                ,
+    'densely dashed'         :  (0, (5, 1))                ,
+    'loosely dashdotted'     :  (0, (3, 10, 1, 10))        ,
+    'dashdotted'             :  (0, (3, 5, 1, 5))          ,
+    'densely dashdotted'     :  (0, (3, 1, 1, 1))          ,
+    'dashdotdotted'          :  (0, (3, 5, 1, 5, 1, 5))    ,
+    'loosely dashdotdotted'  :  (0, (3, 10, 1, 10, 1, 10)) ,
+    'densely dashdotdotted'  :  (0, (3, 1, 1, 1, 1, 1))    ,
+}
 
 #GLOBAL INITIAL FONT SIZES
 #default font sizes
