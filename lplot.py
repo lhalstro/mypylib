1312cac38cfecf613614f4bb6fa92a5ccd4f31ba
lhalstro <logan.halstrom@nasa.gov>
2025-06-10 10:57:47 -0700

    lplot: option for SaveFig to take figure object

--- lplot.py
+++ lplot.py
@@ -1355,13 +1355,14 @@ def GetPlotBbox(ypad=0.5, xpad=0, shft=0.1, offtop=0.5):
 
     return bbox
 
-def SavePlot(savename, overwrite=1, bbox='tight', pad=None, **kwargs):
+def SavePlot(savename, overwrite=1, bbox='tight', pad=None, fig=None, **kwargs):
     """Save file given save path.  Do not save if file exists
     or if variable overwrite is 1
     bbox --> 'tight' for tight border (best for individual plots)
              'fixed' for fixed-size border (best for plots that need to be same size)
              'fixedsquare' same as 'fixed' but final shape is square, not rect
     pad  --> lower left corner padding for 'fixed' bbox [0.5] or padding on all sides for 'tight' [0.1] (inches)
+    fig  --> matplotlib.Figure object to use intead of current figure [None]
     kwargs: matplotlib.pyplot.savefig kwargs e.g. tranparent=True for transparent background
     """
 
@@ -1398,7 +1399,10 @@ def SavePlot(savename, overwrite=1, bbox='tight', pad=None, **kwargs):
             #top is too high, subtrack some height
         bbox = GetPlotBbox(ypad=pad, xpad=0, shft=shft, offtop=0.5)
 
-    plt.savefig(savename, bbox_inches=bbox, pad_inches=pad, **kwargs)
+    if fig is not None:
+        fig.savefig(savename, bbox_inches=bbox, pad_inches=pad, **kwargs)
+    else:
+        plt.savefig(savename, bbox_inches=bbox, pad_inches=pad, **kwargs)
     # plt.savefig(savename, bbox_inches='tight', transparent=trans)
     # plt.close()
 
